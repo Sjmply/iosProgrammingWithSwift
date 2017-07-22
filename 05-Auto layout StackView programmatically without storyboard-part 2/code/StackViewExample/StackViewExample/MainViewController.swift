5add83cc6879ae2a9154150817f60936c5285b43
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-07-22 15:46:42 +0700

    update

--- 05-Auto layout StackView programmatically without storyboard-part 2/code/StackViewExample/StackViewExample/MainViewController.swift
+++ 05-Auto layout StackView programmatically without storyboard-part 2/code/StackViewExample/StackViewExample/MainViewController.swift
@@ -17,6 +17,8 @@ class MainViewController: UIViewController {
     
     var stackView11:UIStackView = UIStackView()
     
+    var stackView12:UIStackView = UIStackView()
+    
     var txtViewDescription: UITextView = UITextView()
     
     var stackView2:UIStackView = UIStackView()
@@ -80,16 +82,59 @@ class MainViewController: UIViewController {
         
         //Autolayout for stackView11
         stackView11.translatesAutoresizingMaskIntoConstraints = false
-//        stackView11.widthAnchor.constraint(equalToConstant: 50).isActive = true
-//        stackView11.heightAnchor.constraint(equalToConstant: 50).isActive = true
         stackView1.addArrangedSubview(stackView11)
+        stackView11.axis = .vertical
+        stackView11.alignment = .leading
+        stackView11.distribution = .fillEqually
+        stackView11.spacing = 10
+        
+        let labelFirst = UILabel()
+        labelFirst.translatesAutoresizingMaskIntoConstraints = false
+        labelFirst.text = "First"
+        stackView11.addArrangedSubview(labelFirst)
+        let labelMiddle = UILabel()
+        labelMiddle.translatesAutoresizingMaskIntoConstraints = false
+        labelMiddle.text = "Middle"
+        stackView11.addArrangedSubview(labelMiddle)
+        let labelLast = UILabel()
+        labelLast.translatesAutoresizingMaskIntoConstraints = false
+        labelLast.text = "Last"
+        stackView11.addArrangedSubview(labelLast)
+        
+        stackView11.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        //Autolayout for stackView12
+        stackView12.translatesAutoresizingMaskIntoConstraints = false
+        stackView1.addArrangedSubview(stackView12)
+        stackView12.axis = .vertical
+        stackView12.alignment = .fill
+        stackView12.distribution = .equalSpacing
+        stackView12.spacing = 0
+        
+        let txtFirst = UITextField()
+        txtFirst.translatesAutoresizingMaskIntoConstraints = false
+        txtFirst.placeholder = "First"
+        txtFirst.borderStyle = .roundedRect
+        stackView12.addArrangedSubview(txtFirst)
+        
+        let txtMiddle = UITextField()
+        txtMiddle.translatesAutoresizingMaskIntoConstraints = false
+        txtMiddle.placeholder = "Middle"
+        txtMiddle.borderStyle = .roundedRect
+        stackView12.addArrangedSubview(txtMiddle)
+        
+        let txtLast = UITextField()
+        txtLast.translatesAutoresizingMaskIntoConstraints = false
+        txtLast.placeholder = "Last"
+        txtLast.borderStyle = .roundedRect
+        stackView12.addArrangedSubview(txtLast)
         
         
     }
     private func autolayoutTextViewDescription() {
         mainStackView.addArrangedSubview(txtViewDescription)
         txtViewDescription.text = "Your description..."
-        txtViewDescription.font = txtViewDescription.font?.withSize(17)
+        txtViewDescription.font = txtViewDescription.font?.withSize(15)
         txtViewDescription.backgroundColor = UIColor(colorLiteralRed: 234.0 / 255.0, green: 221.0 / 255.0, blue: 209.0 / 255.0, alpha: 1)
         txtViewDescription.translatesAutoresizingMaskIntoConstraints = false
     }
@@ -102,10 +147,10 @@ class MainViewController: UIViewController {
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        mainStackView.changeBackgroundColor(color: UIColor.cyan)
-        stackView1.changeBackgroundColor(color: UIColor.orange)
+//        mainStackView.changeBackgroundColor(color: UIColor.cyan)
+//        stackView1.changeBackgroundColor(color: UIColor.orange)
         stackView2.changeBackgroundColor(color: UIColor.blue)
-        stackView11.changeBackgroundColor(color: UIColor.magenta)        
+//        stackView12.changeBackgroundColor(color: UIColor.magenta)
         print("x123")
     }
     
