a3f288921e392636a9f24e35b0d2397661083dec
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-07-22 15:05:34 +0700

    update

--- 04-Auto layout StackView programmatically without storyboard-part 1/code/StackViewExample/StackViewExample/MainViewController.swift
+++ 04-Auto layout StackView programmatically without storyboard-part 1/code/StackViewExample/StackViewExample/MainViewController.swift
@@ -10,25 +10,22 @@ import UIKit
 
 class MainViewController: UIViewController {
     var mainStackView:UIStackView = UIStackView()
-    //var mainStackView:UIView = UIView()//Use this line for debugging/testing , because setting stackview's background is complicated !
     
-    //    var stackView1:UIStackView = UIStackView()
-    var stackView1:UIView = UIView()//Use this line for debugging/testing , because setting stackview's background is complicated !
+    var stackView1:UIStackView = UIStackView()
     
     var txtViewDescription: UITextView = UITextView()
     
-    //    var stackView2:UIStackView = UIStackView()
-    var stackView2:UIView = UIView()//Use this line for debugging/testing , because setting stackview's background is complicated !
-
-
+    var stackView2:UIStackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
         self.autolayoutMainStackView()
         self.autolayoutStackView1()
-        self.autolayoutStackView2()
         self.autolayoutTextViewDescription()
+        self.autolayoutStackView2()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,8 +34,8 @@ class MainViewController: UIViewController {
     }
     private func autolayoutMainStackView() {
         view.addSubview(mainStackView)
-        mainStackView.backgroundColor = .blue
         mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        /*
         NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[mainStackView]-10-|",
                                                                    options: [],
                                                                    metrics: nil,
@@ -47,54 +44,45 @@ class MainViewController: UIViewController {
                                                                    options: [],
                                                                    metrics: nil,
                                                                    views: ["mainStackView": mainStackView]))
-        mainStackView.alignment = .leading //leading = "left"
-        mainStackView.distribution = .fillProportionally
+         */
+        //leadingAnchor && leftAnchor ?
+        mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        mainStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
+        mainStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10).isActive = true
+        
+        mainStackView.axis = .vertical
+        mainStackView.alignment = .fill
+        mainStackView.distribution = .fill
+        mainStackView.spacing = 20
     }
     private func autolayoutStackView1() {
-        mainStackView.addSubview(stackView1)
+        mainStackView.addArrangedSubview(stackView1)
         stackView1.translatesAutoresizingMaskIntoConstraints = false
-        stackView1.backgroundColor = UIColor.cyan
-        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[stackView1(100)]",
-                                                                   options: [],
-                                                                   metrics: nil,
-                                                                   views: ["stackView1": stackView1]))
-        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[stackView1]-|",
-                                                                   options: [],
-                                                                   metrics: nil,
-                                                                   views: ["stackView1": stackView1]))
+        stackView1.heightAnchor.constraint(equalToConstant: 100).isActive = true
     }
-    private func autolayoutStackView2() {
-        mainStackView.addSubview(stackView2)
-        stackView2.translatesAutoresizingMaskIntoConstraints = false
-        stackView2.backgroundColor = UIColor.blue
-        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "V:[stackView2(44)]-0-|",
-                                                                   options: [],
-                                                                   metrics: nil,
-                                                                   views: ["stackView2": stackView2]))
-        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[stackView2]-|",
-                                                                   options: [],
-                                                                   metrics: nil,
-                                                                   views: ["stackView2": stackView2]))
-    }
-    
     private func autolayoutTextViewDescription() {
-        mainStackView.addSubview(txtViewDescription)
+        mainStackView.addArrangedSubview(txtViewDescription)
         txtViewDescription.text = "Your description..."
+        txtViewDescription.font = txtViewDescription.font?.withSize(17)
         txtViewDescription.backgroundColor = UIColor(colorLiteralRed: 234.0 / 255.0, green: 221.0 / 255.0, blue: 209.0 / 255.0, alpha: 1)
         txtViewDescription.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "V:[stackView1]-[txtViewDescription]-[stackView2]",
-                                                                   options: [],
-                                                                   metrics: nil,
-                                                                   views: ["txtViewDescription": txtViewDescription,
-                                                                           "stackView1": stackView1,
-                                                                           "stackView2": stackView2
-            ]))
-        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[txtViewDescription]-|",
-                                                                   options: [],
-                                                                   metrics: nil,
-                                                                   views: ["txtViewDescription": txtViewDescription
-            ]))
-
+    }
+    
+    private func autolayoutStackView2() {
+        mainStackView.addArrangedSubview(stackView2)
+        stackView2.translatesAutoresizingMaskIntoConstraints = false
+        stackView2.backgroundColor = UIColor.blue
+        stackView2.heightAnchor.constraint(equalToConstant: 44).isActive = true
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        mainStackView.changeBackgroundColor(color: UIColor.cyan)
+        stackView1.changeBackgroundColor(color: UIColor.orange)
+        stackView2.changeBackgroundColor(color: UIColor.blue)
+        print("x123")
     }
     
 }
+
+
