7f060172c08c43d33764f3ee769ca7e4123269fc
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-07-26 06:58:16 +0700

    update

--- 09-UICollectionView basics with Xcode 9 and Swift 4/code/CollectionViewExample/CollectionViewExample/AppDelegate.swift
+++ 09-UICollectionView basics with Xcode 9 and Swift 4/code/CollectionViewExample/CollectionViewExample/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  CollectionViewExample
 //
-//  Created by Nguyen Duc Hoang on 7/24/17.
+//  Created by Nguyen Duc Hoang on 7/26/17.
 //  Copyright © 2017 Nguyen Duc Hoang. All rights reserved.
 //
 
--- 09-UICollectionView basics with Xcode 9 and Swift 4/code/CollectionViewExample/CollectionViewExample/MainViewController.swift
+++ 09-UICollectionView basics with Xcode 9 and Swift 4/code/CollectionViewExample/CollectionViewExample/MainViewController.swift
@@ -2,25 +2,24 @@
 //  MainViewController.swift
 //  CollectionViewExample
 //
-//  Created by Nguyen Duc Hoang on 7/24/17.
+//  Created by Nguyen Duc Hoang on 7/26/17.
 //  Copyright © 2017 Nguyen Duc Hoang. All rights reserved.
 //
 
 import UIKit
 
 class MainViewController: UIViewController,
-    UICollectionViewDelegate, UICollectionViewDataSource,
-    UICollectionViewDelegateFlowLayout {
+    UICollectionViewDelegate,
+    UICollectionViewDataSource,
+    UICollectionViewDelegateFlowLayout
+{
     var collectionView: UICollectionView?
-    
     let cellId = "ExampleCell"
     let cellSpacing:CGFloat = 10
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout())
-        //auto layout collectionView
         collectionView?.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(collectionView!)
         collectionView?.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
@@ -36,17 +35,15 @@ class MainViewController: UIViewController,
         collectionViewFlowLayout.minimumInteritemSpacing = 10
         collectionViewFlowLayout.minimumLineSpacing = 10
         
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "ExampleCell")
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
         collectionView?.delegate = self
         collectionView?.dataSource = self
     }
-
-    //    Creating Cells and Supplementary Views
     //UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 100
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
         cell.backgroundColor = indexPath.row % 2 == 0 ? .cyan: .orange
@@ -54,14 +51,16 @@ class MainViewController: UIViewController,
         cell.selectedBackgroundView?.backgroundColor = .red
         return cell
     }
-    
     //UICollectionViewDelegateFlowLayout
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 //        return CGSize(width: 100, height: 100)
+        
 //        let width = (UIScreen.main.bounds.size.width - 3 * cellSpacing) / 2
 //        let height = width
+//        return CGSize(width: width, height: height)
         let width = (UIScreen.main.bounds.size.width - 3 * cellSpacing) / 2
         let height = (UIScreen.main.bounds.size.height - 4 * cellSpacing) / 3
         return CGSize(width: width, height: height)
     }
+    
 }
