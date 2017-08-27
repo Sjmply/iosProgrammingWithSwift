57f3a2db76b71137ed23135dde70885e117d6a52
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-08-27 15:48:30 +0700

    update

---
+++ 25-Auto layout UITableView and UITableViewCell in Storyboard Xcode 9/code/TableViewExample/TableViewExample/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  TableViewExample
+//
+//  Created by Nguyen Duc Hoang on 8/27/17.
+//  Copyright © 2017 Nguyen Duc Hoang. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    //MARK - UITableViewDataSource & UITableViewDelegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FoodTableViewCell") as! UITableViewCell
+        return cell
+    }
+}
