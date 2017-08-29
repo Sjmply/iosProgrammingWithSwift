6769d54a1fa7abdce84e1a2337137acd3f054cca
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-08-29 10:53:33 +0700

    update

--- 30-Update or delete a row in UITableView Swift 4/code/TableViewExample/TableViewExample/MainViewController.swift
+++ 30-Update or delete a row in UITableView Swift 4/code/TableViewExample/TableViewExample/MainViewController.swift
@@ -37,4 +37,73 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         cell.lblDescription?.text = food.foodDescription
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let food = foods[indexPath.row]
+        let editAction = UITableViewRowAction(style: .default, title: "Edit") { (action, indexPath) in
+            self.updateAction(food: food, indexPath: indexPath)
+        }
+        editAction.backgroundColor = .blue
+ 
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (action, indexPath) in
+            self.deleteAction(food: food, indexPath: indexPath)
+        }
+        deleteAction.backgroundColor = .red
+        return [deleteAction, editAction]
+    }
+    private func updateAction(food: Food, indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Update",
+                                      message: "Update a food",
+                                      preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) { action in
+                                        guard let textField = alert.textFields?.first else {
+                                            return
+                                        }
+                                        if let textToEdit = textField.text {
+                                            if textToEdit.count == 0 {
+                                                return
+                                            }
+                                            food.foodName = textToEdit
+                                            self.tableView?.beginUpdates()
+                                            self.tableView?.reloadRows(at: [indexPath], with: .automatic)
+                                            self.tableView?.endUpdates()
+                                        } else {
+                                            return
+                                        }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel ",
+                                         style: .default)
+        alert.addTextField()
+        guard let textField = alert.textFields?.first else {
+            return
+        }
+        textField.placeholder = "Change your food's name "
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    private func deleteAction(food: Food, indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Delete",
+                                      message: "Are you sure you want to delete food ?",
+                                      preferredStyle: .alert)
+        let deleteAction = UIAlertAction(title: "Yes",
+                                         style: .default) { action in
+                                            foods.remove(at: indexPath.row)
+                                            self.tableView?.deleteRows(at: [indexPath], with: .bottom)
+                                            return
+                                        }
+        let cancelAction = UIAlertAction(title: "No",
+                                         style: .default)
+        alert.addAction(deleteAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if(editingStyle == .delete) {
+            
+        }
+    }
 }
