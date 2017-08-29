1d9eb7e7cd1395a25b7e81ce6bdcc9b51f6b973e
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-08-29 09:08:06 +0700

    update

---
+++ 30-Update or delete a row in UITableView Swift 4/code/TableViewExample/TableViewExample/Data/FoodData.swift
@@ -0,0 +1,18 @@
+//
+//  FoodData.swift
+//  TableViewExample
+//
+//  Created by Nguyen Duc Hoang on 8/29/17.
+//  Copyright © 2017 Nguyen Duc Hoang. All rights reserved.
+//
+
+import Foundation
+var foods = [
+    Food(imageName: "cream-tea", foodName: "Cream Tea", rating: 2, foodDescription: "This is a cup of cream tea"),
+    Food(imageName: "japanese-salad", foodName: "Japanese Salad", rating: 2, foodDescription: "Very delicious Japanese Salad"),
+    Food(imageName: "kimchi", foodName: "Korean Kimchi", rating: 3, foodDescription: "Traditional Korean Food"),
+    Food(imageName: "mushroom", foodName: "Fresh mushroom", rating: 4, foodDescription: "Fresh mushroom with vegetables"),
+    Food(imageName: "oysters", foodName: "Oysters", rating: 5, foodDescription: "Oysters with ice rock"),
+    Food(imageName: "salad-mushroom", foodName: "Multiple salad", rating: 1, foodDescription: "Salad mixed with mushroom"),
+    Food(imageName: "vegetable", foodName: "Vegetable", rating: 5, foodDescription: "Fresh vegetables")
+]
